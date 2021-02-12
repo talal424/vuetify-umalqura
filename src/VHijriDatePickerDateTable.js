afd42e0356482a9f19539819fcc0469cfe7e6eaa
talal424 <shaheen.t@gmail.com>
2021-02-13 02:24:05 +0300

    vuetify version bump & added show-adjacent-months prop

--- src/VHijriDatePickerDateTable.js
+++ src/VHijriDatePickerDateTable.js
@@ -28,8 +28,15 @@ export default {
             if (this.showWeek) {
                 rows.push(this.genWeekNumber(this.getWeekNumber(1)));
             }
+
+            const prevMonthYear = this.displayedMonth === 1 ? this.displayedYear - 1 : this.displayedYear;
+            const prevMonth = this.displayedMonth === 1 ? 12 : this.displayedMonth - 1;
+            const firstDayFromPreviousMonth = daysInMonth(prevMonthYear, prevMonth);
     
-            while (day--) rows.push(this.$createElement('td'));
+            while (day--) {
+                const date = `${prevMonthYear}-${pad(prevMonth)}-${pad(firstDayFromPreviousMonth - day)}`;
+                rows.push(this.$createElement('td', this.showAdjacentMonths ? [this.genButton(date, true, 'date', this.formatter, true)] : []));
+            }
     
             for (day = 1; day <= daysInCurrentMonth; day++) {
                 const date = `${this.displayedYear}-${pad(this.displayedMonth)}-${pad(day)}`;
@@ -43,6 +50,15 @@ export default {
                         rows.push(this.genWeekNumber(this.getWeekNumber((day + 7) > daysInCurrentMonth ? daysInCurrentMonth : (day + 7))));
                     }
                 }
+            }
+
+            const nextMonthYear = this.displayedMonth === 12 ? this.displayedYear + 1 : this.displayedYear;
+            const nextMonth = this.displayedMonth === 12 ? 1 : this.displayedMonth + 1;
+            let nextMonthDay = 1;
+
+            while (rows.length < 7) {
+                const date = `${nextMonthYear}-${pad(nextMonth)}-${pad(nextMonthDay++)}`;
+                rows.push(this.$createElement('td', this.showAdjacentMonths ? [this.genButton(date, true, 'date', this.formatter, true)] : []));
             }
     
             if (rows.length) {
